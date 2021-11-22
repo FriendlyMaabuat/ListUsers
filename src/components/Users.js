2322cbcdab2f5c241ea60b4a682272c17a42a30d
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2021-11-22 15:10:43 +0800

    fix

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,31 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
 export default function Users() {
-  const [arr, changeArr] = useState([])
+  const [arr, changeArr] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://reqres.in/api/users?page=2")
-    .then((response)=>response.json())
-    .then((json)=> changeArr(json))
-  })
-
+      .then((response) => response.json())
+      .then((json) => changeArr(json.data.map((item)=>{
+            return (
+              <div key={item.id}>
+                <figure>
+                  <img src={item.avatar} alt="" />
+                </figure>
+                <p> first name:{item.first_name} </p>
+                <p> last name:{item.last_name} </p>
+                <p> email:{item.email} </p>
+              </div>
+            );
+          })
+        )
+      );
+  }, []);
 
   return (
     <div>
-    <h3>User Page</h3> 
-       {
-           arr["data"].map((item)=>{
-            return(
-                <div key={item.id}>
-                    <figure>
-                        <img src={item.avatar} alt=""/> 
-                    </figure>
-                <p> first name:{item.first_name}  </p>
-                <p> last name:{item.last_name}   </p> 
-                <p> email:{item.email}   </p>
-                <p>===============================</p>
-                </div>
-            )
-        })
-       }
-</div>
-  )
+      <h3>User Page</h3>
+      {arr}
+    </div>
+  );
 }
